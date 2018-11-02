aa40fbcd793d54b8a4927fbdffac28eea12a0123
MrLucasFischer <lucasfischerpt@gmail.com>
2018-11-02 18:07:03 +0000

    ready to implement files

---
+++ spark_dataframe.py
@@ -0,0 +1,7 @@
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark import SparkContext
+
+spark = SparkSession.builder.master('local[*]').appName('taxify').getOrCreate()
+sc = spark.sparkContext

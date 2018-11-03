1fe2b346dbee63e455e04e161162379c9e9f5450
MrLucasFischer <lucasfischerpt@gmail.com>
2018-11-03 16:43:33 +0000

    updated regular spark

--- regular_spark.py
+++ regular_spark.py
@@ -23,7 +23,7 @@ def get_user_options():
                 pickup_correct = True
         except:
             #User didn't sent us a number
-            print("Please insert a number between 1 -265\n")
+            print("Please insert a number between 1 - 265\n")
         
 
     #Continue asking the user until he/she gives us a number between 1 and 265
@@ -34,13 +34,13 @@ def get_user_options():
                 dropoff_correct = True
         except:
             #User didn't sent us a number
-            print("Please insert a number between 1 -265\n")
+            print("Please insert a number between 1 - 265\n")
 
     return(pickup_id, dropoff_id)
 
 
 
-print(get_user_options())
+#print(get_user_options())
 
 try :
     lines = sc.textFile('yellow_tripdata_2018-01_sample.csv') #read csv file (change this to the full dataset instead of just the sample) (this is local to my machine)
@@ -48,8 +48,8 @@ try :
     non_empty_lines = lines.filter(lambda line: len(line) > 0 and line != first_line)  #Filter out empty lines and the first line
     #maybe we filter the lines that don't contain the options the user sent us like the PU_ID, DO_ID etc. 
 
-    organized_lines = non_empty_lines.map(lambda line: (line.split(",")[2], line.split(",")[7], line.split(",")[8], line.split(",")[16])) # (Date, PU_ID, DO_ID, Total_Ammount)
-    grouped = organized_lines.groupBy(lambda tuple: (tuple[0], tuple[1], tuple[2]))
+    organized_lines = non_empty_lines.map(lambda line: ((line.split(",")[2], line.split(",")[7], line.split(",")[8]), line.split(",")[16])) # (Date, PU_ID, DO_ID, Total_Ammount)
+    grouped = organized_lines.groupByKey().mapValues(list)
 
     for a in grouped.take(10):
         #See how to iterate over values

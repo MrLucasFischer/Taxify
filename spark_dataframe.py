e83d23ab5bf8260684fc5a99eeccfead37f8f17b
MrLucasFischer <lucasfischerpt@gmail.com>
2018-11-16 16:46:28 +0000

    updating

--- spark_dataframe.py
+++ spark_dataframe.py
@@ -158,6 +158,11 @@ def get_duration(pick_up_datetime, drop_off_datetime):
     return int((d1 - d2) / 60)
 
 
+def test(input_date_time):
+    print(input_date_time)
+    return ""
+
+
 def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user_hour = 0, user_minutes = 21, filename = 'yellow_tripdata_2018-01_sample.csv'):
     try :
         lines = sc.textFile(filename) #read csv file (change this to the full dataset instead of just the sample) (this is local to my machine)
@@ -165,9 +170,17 @@ def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user
         
         #Filtering out the first line, empty lines
         non_empty_lines = lines.filter(lambda line: len(line) > 0 and line != first_line)
+        fields = non_empty_lines.map(lambda line : Row(pickup_datetime = line.split(',')[1], dropoff_datetime = line.split(',')[2], pickup_id = line.split(',')[7], dropoff_id = line.split(',')[8], amount = line.split(',')[16]));
+        fields_df = spark.createDataFrame(fields)
+        filtered = fields_df.where((col("pickup_id") == str(user_puid)) & (col("dropoff_id") == str(user_doid)))
+        
+        filter_udf = udf(lambda r: filter_dates(r, user_weekday, user_hour, user_minutes), BooleanType())
+
+        filter_test = filtered.filter(filter_udf(filtered.pickup_datetime))
+        filter_test.show(10)
 
         #Filter out lines that don't match user's pickup-ID and dropoff-ID
-        lines_with_piud_doid = non_empty_lines.filter(lambda line: line.split(",")[7] == str(user_puid) and line.split(",")[8] == str(user_doid))
+        # lines_with_piud_doid = nocoln_empty_lines.filter(lambda line: line.split(",")[7] == str(user_puid) and line.split(",")[8] == str(user_doid))
 
         # # My attempt to transform above code to use dataFrames
         # # Get fields from lines
@@ -176,27 +189,27 @@ def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user
         # df_pu_do_dt_id_and_amount = spark.createDataFrame( fields_in_lines )
         # df_pu_do_dt_id_and_amount.show(10)
         # #Filter out rows that don't match user's pickup-ID and dropoff-ID from dataFrame
-        # df_pu_do_dt_id_and_amount = df_pu_do_dt_id_and_amount.where( col('pu_id') == str(user_puid) ).where( col('do_id') == str(user_doid) )
+        # df_pu_do_dt_id_and_amount = df_pu_do_dt_id_and_amount.where( col('pu_id') == str(user_puid)).where( col('do_id') == str(user_doid) )
         # df_pu_do_dt_id_and_amount.show(10)
 
         #Filter out lines that are not within the user's time radius
-        lines_with_hour = lines_with_piud_doid.filter(lambda line: filter_dates(line.split(",")[1], user_weekday, user_hour, user_minutes))
+        # lines_with_hour = lines_with_piud_doid.filter(lambda line: filter_dates(line.split(",")[1], user_weekday, user_hour, user_minutes))
 
         # ((weekday, hour, minute, PU_ID, DO_ID), (vendorID, duration, Total_Ammount))
-        organized_lines = lines_with_hour.map(lambda line: create_key_value(line))
+        # organized_lines = lines_with_hour.map(lambda line: create_key_value(line))
         
         #Reduce everything by key returning a 3 column tuple
         #(vendor_ID, list of durations, list of amounts)
-        grouped = organized_lines.reduceByKey(lambda accum, elem: (accum[0] + elem[0], accum[1] + elem[1]))
+        # grouped = organized_lines.reduceByKey(lambda accum, elem: (accum[0] + elem[0], accum[1] + elem[1]))
         
-        for k, v in grouped.collect():
-            # pick_up_taxi_zones = locations.loc[locations["LocationID"] == int(k[1]), ["Zone", "Borough"]]
-            # drop_off_taxi_zones = locations.loc[locations["LocationID"] == int(k[2]), ["Zone", "Borough"]]
-            average_duration = np.mean(v[0])
-            average_amount = np.mean(v[1])
-            print(v)
-            print("\nFor {} at {0:02d}:{0:02d}, a trip from (ID: {}) to (ID: {}) takes an average of {} minutes and costs about ${}"\
-            .format(k[0], user_hour, user_minutes,k[1], k[2], average_duration, average_amount))
+        # for k, v in grouped.collect():
+        #     # pick_up_taxi_zones = locations.loc[locations["LocationID"] == int(k[1]), ["Zone", "Borough"]]
+        #     # drop_off_taxi_zones = locations.loc[locations["LocationID"] == int(k[2]), ["Zone", "Borough"]]
+        #     average_duration = np.mean(v[0])
+        #     average_amount = np.mean(v[1])
+        #     print(v)
+        #     print("\nFor {} at {0:02d}:{0:02d}, a trip from (ID: {}) to (ID: {}) takes an average of {} minutes and costs about ${}"\
+        #     .format(k[0], user_hour, user_minutes,k[1], k[2], average_duration, average_amount))
 
         sc.stop()
     except:
@@ -205,6 +218,8 @@ def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user
 
 
 
-user_weekday, user_puid, user_doid, user_hour, user_minutes = get_user_options()
+# user_weekday, user_puid, user_doid, user_hour, user_minutes = get_user_options()
 
-create_inverted_index(int(user_weekday), user_puid, user_doid, int(user_hour), int(user_minutes))
+# create_inverted_index(int(user_weekday), user_puid, user_doid, int(user_hour), int(user_minutes))
+
+create_inverted_index()

acbeeba86a36236013e09a90e3f80a2175a75308
MrLucasFischer <lucasfischerpt@gmail.com>
2018-11-17 13:16:41 +0000

    Client for spark rdd

--- regular_spark.py
+++ regular_spark.py
@@ -1,4 +1,5 @@
 import pyspark
+from pyspark.sql import SparkSession
 import traceback
 import datetime
 from datetime import datetime as dt
@@ -6,112 +7,13 @@ import calendar
 import time
 import numpy as np
 
-sc = pyspark.SparkContext('local[*]') #Create spark context
+spark = SparkSession.builder.master('local[*]').appName('taxify').getOrCreate()
+sc = spark.sparkContext
 
 #PU/DO zone ids range from 1 to 265, see taxi_zone_lookup.csv
 #date(position 1, maybe split datetime into weekday and time), PU_ID (position 7), DO_ID (position 8), totalammount (position 16)
 
 #Main implementation
-
-def get_user_options():
-    """
-        Function that gets all the users input for creating the inverted index.
-        This function gets the desired weekday, time, pickup and dropoff zone
-    """
-
-    pickup_correct = False
-    dropoff_correct = False
-    weekday_correct = False
-    time_correct = False
-    pickup_id = ""
-    dropoff_id = ""
-    weekday = ""
-    hour = ""
-    minutes = ""                                                
-
-    #Continue asking the user until he/she gives us a weekday
-    while(not weekday_correct):
-        weekday = input("\nPlease insert you weekday (1- Monday, 2- Tuesday, ..., 7- Sunday): ")
-        try:
-            if(int(weekday) >= 1 and int(weekday) <= 7):
-                weekday_correct = True
-        except:
-            #User didn't sent us a number
-            print("\nPlease insert a number between 1 - 7\n")
-
-    #Continue asking the user until he/she gives us an hour
-    while(not time_correct):
-        time_input = input("\nPlease insert the desired time (hh:mm): ")
-        try:
-            user_time = time.strptime(time_input, '%H:%M') # Check time is in proper format
-            time_correct = True
-            hour = user_time.tm_hour #Get hour
-            minutes = user_time.tm_min #Get minutes
-
-        except:
-            #User didn't sent us a number
-            print("\nPlease insert a time in the format hh:mm where hh (00-23) and mm (00:59) \n")    
-
-
-
-    #Continue asking the user until he/she gives us a number between 1 and 265
-    while(not pickup_correct):
-        pickup_id = input("\nPlease insert you Pick-Up location ID (1 - 265): ")
-        try:
-            if(int(pickup_id) >= 1 and int(pickup_id) <= 265):
-                pickup_correct = True
-        except:
-            #User didn't sent us a number
-            print("\nPlease insert a number between 1 - 265\n")
-
-        
-
-    #Continue asking the user until he/she gives us a number between 1 and 265
-    while(not dropoff_correct):
-        dropoff_id = input("\nPlease insert you Drop-Off location ID (1 - 265): ")
-        try:
-            if(int(dropoff_id) >= 1 and int(dropoff_id) <= 265):
-                dropoff_correct = True
-        except:
-            #User didn't sent us a number
-            print("\nPlease insert a number between 1 - 265\n")
-
-
-    return(weekday, pickup_id, dropoff_id, hour, minutes)
-
-
-def filter_dates(input_date_time, user_weekday, user_hour, user_minutes):
-    """
-        Predicate function that returns true if input_date_time is within 30 minutes radius of user's desired time, false otherwise
-
-        Params:
-            input_date - String in YYYY-MM-DD HH:MM format
-            user_weekday - Integer ranging from 1 to 7 representing the weekday
-            user_hour - Integer representing the hour
-            user_minutes - Integer representing the minutes
-
-        Returns:
-            True if input_date_time is within 30 minutes radius of user's desired time, false otherwise
-    """
-
-    #First check if input_date_time week day is at the maximum one more day than users desired time
-    date_obj = dt.strptime(input_date_time, '%Y-%m-%d %H:%M:%S')
-
-    input_weekday = date_obj.weekday()
-    user_weekday -= 1   #since input_weekday is between [0, 6] we need to subtract 1 to our user_weekday
-
-    input_date = input_date_time[0:10] #Getting the characters that represent the date
-    user_date = dt.strptime(input_date + " {}:{}:00".format(user_hour, user_minutes), '%Y-%m-%d %H:%M:%S') #Creating a new date time object with the date of the input date, and time of the user
-
-    if(user_hour == 23 and user_minutes > 29):
-        user_date = dt.strptime(input_date + " {}:{}:00".format(user_hour, user_minutes), '%Y-%m-%d %H:%M:%S') - datetime.timedelta(days = 1)
-
-    if(input_weekday == user_weekday or (input_weekday == 0 and user_weekday == 6) or (input_weekday == user_weekday + 1)):
-        time_plus_30_min = (user_date + datetime.timedelta(minutes = 30))
-        return user_date <= date_obj <= time_plus_30_min
-    else:
-        return False
-
 
 def create_key_value(line):
     """
@@ -150,7 +52,7 @@ def get_duration(pick_up_datetime, drop_off_datetime):
     return int((d1 - d2) / 60)
 
 
-def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user_hour = 0, user_minutes = 21, filename = 'yellow_tripdata_2018-01_sample.csv'):
+def create_inverted_index(filename = 'yellow_tripdata_2018-01_sample.csv'):
     """
         Function that creates the inverted index. This function holds the main implementation of spark code to create the inverted index
 
@@ -164,7 +66,7 @@ def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user
     """
 
     try :
-        beforeT = dt.now()
+        time_before = dt.now()
         lines = sc.textFile(filename) #read csv file (change this to the full dataset instead of just the sample)
         first_line = lines.first()
 
@@ -179,12 +81,13 @@ def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user
         grouped = organized_lines.reduceByKey(lambda accum, elem: (accum[0] + elem[0], accum[1] + elem[1]))
 
         #Obtain the average of the 
-        grouped_with_averages = grouped.mapValues(lambda tup: (np.mean(tup[0]), np.mean(tup[1]))).collect()
+        grouped_with_averages = grouped.mapValues(lambda tup: (np.mean(tup[0]), np.mean(tup[1])))
+        
+        grouped_with_averages.saveAsTextFile("spark_rdd_results/inverted_index.txt")
 
-        afterT = dt.now()
-        diffT = afterT - beforeT
-        max_time = (diffT.microseconds / 1000)
-        print("Execution time {}".format(max_time))
+        time_after = dt.now()
+        seconds = (time_after - time_before).total_seconds()
+        print("Execution time {} seconds".format(seconds))
 
         sc.stop()
     except:
@@ -192,8 +95,4 @@ def create_inverted_index(user_weekday = 1, user_puid = 41, user_doid = 24, user
         sc.stop()
 
 
-
-# user_weekday, user_puid, user_doid, user_hour, user_minutes = get_user_options()
-
-# create_inverted_index(int(user_weekday), user_puid, user_doid, int(user_hour), int(user_minutes))
 create_inverted_index()
---
+++ spark_rdd_client.py
@@ -0,0 +1,116 @@
+import pyspark
+from pyspark.sql import SparkSession
+import traceback
+import calendar
+
+spark = SparkSession.builder.master('local[*]').appName('taxify').getOrCreate()
+sc = spark.sparkContext
+
+def get_user_options():
+    """
+        Function that gets all the users input for creating the inverted index.
+        This function gets the desired weekday, time, pickup and dropoff zone
+    """
+
+    pickup_correct = False
+    dropoff_correct = False
+    weekday_correct = False
+    time_correct = False
+    pickup_id = ""
+    dropoff_id = ""
+    weekday = ""
+    hour = ""                                            
+
+    #Continue asking the user until he/she gives us a weekday
+    while(not weekday_correct):
+        weekday = input("\nPlease insert you weekday (1- Monday, 2- Tuesday, ..., 7- Sunday): ")
+        try:
+            if(int(weekday) >= 1 and int(weekday) <= 7):
+                weekday_correct = True
+            else:
+                print("\nPlease insert a number between 1 - 7\n")
+        except:
+            #User didn't sent us a number
+            print("\nPlease insert a number between 1 - 7\n")
+
+    #Continue asking the user until he/she gives us an hour
+    while(not time_correct):
+        time_input = input("\nPlease insert the desired hour: ")
+        try:
+            if(int(time_input) >= 0 and int(time_input) <=23):
+                time_correct = True
+                hour = time_input
+            else:
+                print("\Hour should be an integer between 0 and 23 \n")
+        except:
+            #User didn't sent us a number
+            print("\Hour should be an integer between 0 and 23 \n")    
+
+
+
+    #Continue asking the user until he/she gives us a number between 1 and 265
+    while(not pickup_correct):
+        pickup_id = input("\nPlease insert you Pick-Up location ID (1 - 265): ")
+        try:
+            if(int(pickup_id) >= 1 and int(pickup_id) <= 265):
+                pickup_correct = True
+            else:
+                print("\nPlease insert a number between 1 - 265\n")
+        except:
+            #User didn't sent us a number
+            print("\nPlease insert a number between 1 - 265\n")
+
+        
+
+    #Continue asking the user until he/she gives us a number between 1 and 265
+    while(not dropoff_correct):
+        dropoff_id = input("\nPlease insert you Drop-Off location ID (1 - 265): ")
+        try:
+            if(int(dropoff_id) >= 1 and int(dropoff_id) <= 265):
+                dropoff_correct = True
+            else:
+                print("\nPlease insert a number between 1 - 265\n")
+        except:
+            #User didn't sent us a number
+            print("\nPlease insert a number between 1 - 265\n")
+
+
+    return(weekday, pickup_id, dropoff_id, hour)
+
+
+
+def transform_line(line):
+    """
+        Function that transforms every String line in the inverted index into a searchable array
+    """
+    stripped_line = line.replace("(", "").replace(")", "").replace(" ", "").replace("\'", "")
+    return stripped_line.split(",")
+
+
+    
+def search_index(user_weekday = 1, user_puid = 41, user_doid = 24, user_hour = 0, filename = "spark_rdd_results/inverted_index.txt"):
+    try:
+        lines = sc.textFile(filename) #read the inverted index created previously
+        
+        #Transform each line into an array
+        transform_lines = lines.map(lambda line: transform_line(line)) 
+        
+        #Filter out the lines that don't match the user's desired pick up zone and drop off zone
+        lines_with_puid_doid = transform_lines.filter(lambda arr: arr[2] == str(user_puid) and arr[3] == str(user_doid))
+        
+        #Filter out the lines that don't match the user's desired weekday
+        lines_with_weekday = lines_with_puid_doid.filter(lambda arr: arr[0] == (calendar.day_name[user_weekday - 1]).lower())
+        
+        #Filter out the lines that don't match user's desired hour
+        lines_with_hour = lines_with_weekday.filter(lambda arr: int(arr[1]) == user_hour)
+        
+        for result in lines_with_hour.collect():
+            print(result)
+
+    except:
+        traceback.print_exc()
+        sc.stop()
+
+user_weekday, user_puid, user_doid, user_hour = get_user_options()
+
+search_index(int(user_weekday), user_puid, user_doid, int(user_hour))
